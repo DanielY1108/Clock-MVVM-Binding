9b20d8236ac3e05927d50d129bf051695961e97e
DanielY1108 <scarlet040@gmail.com>
2023-02-14 20:54:17 +0900

    Initial Commit

---
+++ Clock-MVVM-Binding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clock-MVVM-Binding
+//
+//  Created by JINSEOK on 2023/02/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
