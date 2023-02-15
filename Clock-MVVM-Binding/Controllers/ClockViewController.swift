78912ea565389936078b4129d05de92b15f8ae0f
DanielY1108 <scarlet040@gmail.com>
2023-02-15 19:30:25 +0900

    combine 사용

--- Clock-MVVM-Binding/Controllers/ClockViewController.swift
+++ Clock-MVVM-Binding/Controllers/ClockViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 class ClockViewController: UIViewController {
     
@@ -13,28 +14,32 @@ class ClockViewController: UIViewController {
     @IBOutlet weak var observableTimeLabel: UILabel!
     @IBOutlet weak var conbineTimeLabel: UILabel!
     
-    // 1. 뷰 모델 생성(이 때 didSet이 호출해도 동작이 실행되지 않음)
-    // 코드가 먼저 메모리에 올라간 뒤 viewDidLoad를 업데이트 시키키 때문.
+    // 메모리 관리를 위해 취소를 자동으로 해주는 AnyCancellable 만들어 준다.
+    // (뷰컨이 해제되면 자동으로 cancel() 실행)
+    var cancellables: Set<AnyCancellable> = []
+    
     private var viewModel = ClockViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 2. 실행
         setBindings()
         startTimer()
     }
     
-    // 4. 매 초마다 시간을 업데이트 (checkTime 실행)
     func startTimer() {
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-            self.viewModel.checkTime()
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
+            self?.viewModel.checkTime()
         }
     }
     
-    // 3-2. 정의해 둔 bind 메서드를 통해서 text에 업데이트가 됩니다. (초기화된 값을 갖고 업데이트)
     func setBindings() {
-        viewModel.observableTime.bind { [weak self] time in
-            self?.observableTimeLabel.text = time
-        }
+        viewModel.$combineTime
+             // compactMap을 통해 옵셔널을 제거해준다.
+            .compactMap { String($0) }
+            // assign은 keyPath형식으로 객체 접근하여 참조하는 방식이라고 하네요.
+            // conbineTimeLabel의 text에 접근
+            .assign(to: \.text, on: conbineTimeLabel)
+            // 취소할 동작을 Set에 저장
+            .store(in: &cancellables)
     }
 }
--- Clock-MVVM-Binding/ViewModels/ClockModelView.swift
+++ Clock-MVVM-Binding/ViewModels/ClockModelView.swift
@@ -6,19 +6,15 @@
 //
 
 import Foundation
+import Combine
 
 class ClockViewModel {
     
-    // 만들어 놓은 Observable 객체를 생성해줍시다.
-    var observableTime: Observable<String> = Observable("기본값")
- 
-    init() {
-        observableTime.value = Clock.currentTime()
-    }
-    
-    // 5. value를 호출하여 시간을 업데이트되므로 didSet이 실행 됨
-    // Observable은 String타입을 감싼 형태이므로 value를 통해 값을 저장한다.
+    // @Published를 통해 데이터를 제공해 준다.
+    // 연산자 $를 통하여 Published에 속성에 접근할 수 있다
+    @Published var combineTime: String = "Combine"
+
     func checkTime() {
-        observableTime.value = Clock.currentTime()
+        combineTime = Clock.currentTime()
     }
 }
